8dbec658beb805e284040f59e39b11911ffbfc24
Noah Hsu <i@nn.ci>
2025-07-26 03:50:23 +0000

    upload 3.46.0 dist files

--- dist/assets/ResponsiveGrid-c20dba15.js
+++
@@ -1,1 +0,0 @@
-import{a4 as i,x as n,dE as a}from"./index-bcc8ecfe.js";const o=e=>{const r=i("settings_layout");let t;return r==="responsive"&&(t="repeat(auto-fill, minmax(424px, 1fr))"),n(a,{w:"$full",gap:"$2",templateColumns:{"@initial":"1fr","@lg":t},get children(){return e.children}})};export{o as R};
